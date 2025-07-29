19664f826b6f709da6fb214497589bff8bff2014
Subodh <subodhlst@gmail.com>
2025-07-30 04:49:21 +0530

    add scroll Navigation

--- js/script.js
+++ js/script.js
@@ -26,6 +26,33 @@ btnNav.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
+//////////////////////////////////////////////////
+//Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+console.log(allLinks);
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    // Scroll to top
+    if (href === "#") window.scrollTo({ top: 0, behavior: "smooth" });
+
+    //Scroll to other Links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    //Close mobile Navigation
+
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
